c9de161ad486bc83c772973b69a8f09a331f3a0b
Anon Dev <michaelerastus9@gmail.com>
2024-02-29 12:48:00 +0100

    Added viewport

--- resources/views/home/base.blade.php
+++ resources/views/home/base.blade.php
@@ -1,6 +1,7 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
     <!-- Start Meta -->
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
